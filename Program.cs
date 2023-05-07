32c206e5ae921679b02a5ac6b919d23848884dff
Vinicius Chaves <vinciusc7@gmail.com>
2023-05-07 13:19:43 -0300

    Criada classe posição

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " +  p);
+        }
+    }
+}
