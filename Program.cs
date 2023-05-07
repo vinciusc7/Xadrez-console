860c2cbc9f2d7f546b8da42d25055d2a07cea470
Vinicius Chaves <vinciusc7@gmail.com>
2023-05-07 15:35:43 -0300

    Melhorando a implementação de imprimir tabuleiro

--- Program.cs
+++ Program.cs
@@ -6,11 +6,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-        Console.WriteLine(pos);
+        try
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-        Console.WriteLine(pos.toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 2));
 
-        Console.WriteLine(pos.toPosicao);
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+            Tela.imprimirTabuleiro(tab);
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
